1793918ece2988e91a8cd6d25e0b5be47833e2d9
saima umer <saimaumer89@gmail.com>
2024-03-06 01:30:36 +0500

    first commit

---
+++ Q3.js
@@ -0,0 +1,10 @@
+//   QUESTION NO 3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase. 
+//             SOLUTION:
+var fullName = "saima umer";
+//            uppercase:
+console.log(fullName.toUpperCase());
+//            lowercase:
+console.log(fullName.toLowerCase());
+//             titlecase
+console.log(fullName.charAt(0).toUpperCase() + fullName.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,15 @@
+//   QUESTION NO 3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase. 
+
+//             SOLUTION:
+let fullName = "saima umer" ;
+
+//            uppercase:
+console.log(fullName.toUpperCase());
+
+
+//            lowercase:
+console.log(fullName.toLowerCase());
+
+//             titlecase
+console.log(fullName.charAt(0).toUpperCase()+ fullName.slice(1));
